a1263b46ded886698a7c33b40240a3d6d4df75a5
sharma.naman <naman.sharma.ext@veolia.com>
2025-08-13 15:56:16 +0530

    Created VPC,Subnet &RT

---
+++ networking.tf
@@ -0,0 +1,75 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Infra-Vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Infra-Public Subnet ${count.index + 1}"
+  }
+}
+
+
+resource "aws_subnet" "private_subnets" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Infra-Private Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+ tags = {
+   Name = "Infra VPC IG"
+ }
+}
+
+resource "aws_route_table" "private_rt" {
+ vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+  tags = {
+   Name = "Infra-Private-RT"
+ }
+}
+
+resource "aws_route_table" "public_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ tags = {
+   Name = "Infra-Public-RT"
+ }
+} 
+
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_asso" {
+ count = length(var.private_subnet_cidrs)
+ subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+ route_table_id = aws_route_table.private_rt.id
+}
